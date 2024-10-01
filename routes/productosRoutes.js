2ab51155b5a85c4979b88df985d58d53e43c028a
daniloviu21 <daniel.darkwatch@gmail.com>
2024-09-30 22:27:48 -0600

    feat(routes): agregar las rutas para operaciones CRUD de productos

---
+++ routes/productosRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const ProductosController = require('../controllers/productosController');
+
+const router = express.Router();
+
+router.get('/products', ProductosController.getAllProducts);
+router.post('/products', ProductosController.createProduct);
+router.put('/products/:id', ProductosController.updateProduct);
+router.delete('/products/:id', ProductosController.deleteProduct);
+
+module.exports = router;
