deb0694b80e29775c108d218f897a51395fcf12f
daniloviu21 <daniel.darkwatch@gmail.com>
2024-09-30 22:35:19 -0600

    feat: configurar index.js y agregar rutas de productos

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hola a todos");
+const express = require('express')
+const productosRoutes = require('./routes/productosRoutes');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api',productosRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+    console.log(`Server corriendo en el puerto ${PORT}`);
+});
