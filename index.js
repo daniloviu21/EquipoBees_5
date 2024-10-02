312a4f95f50305fb66db3049f09ccc0880284635
daniloviu21 <daniel.darkwatch@gmail.com>
2024-10-01 20:22:43 -0600

    feat: agregar rutas de usuarios al index

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const productosRoutes = require('./routes/productosRoutes');
+const usuariosRoutes = require('./routes/usuariosRoutes');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
 app.use('/api',productosRoutes);
+app.use('/api',usuariosRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
