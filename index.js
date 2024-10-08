a5095a127c1eb310fb7226376843c8076e2332d3
daniloviu21 <daniel.darkwatch@gmail.com>
2024-10-07 21:45:23 -0600

    feat: agregar rutas de servicios al index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const productosRoutes = require('./routes/productosRoutes');
 const usuariosRoutes = require('./routes/usuariosRoutes');
+const serviciosRoutes = require('./routes/serviciosRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 app.use('/api',productosRoutes);
 app.use('/api',usuariosRoutes);
+app.use('/api',serviciosRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
