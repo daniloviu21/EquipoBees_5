7f73d21f1d58f88bb21148fef1ec1a1f807b90a3
daniloviu21 <daniel.darkwatch@gmail.com>
2024-10-08 09:04:24 -0600

    feat: agregar ruta de ventas al index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const productosRoutes = require('./routes/productosRoutes');
 const usuariosRoutes = require('./routes/usuariosRoutes');
 const serviciosRoutes = require('./routes/serviciosRoutes');
+const ventasRoutes = require('./routes/ventasRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use('/api',productosRoutes);
 app.use('/api',usuariosRoutes);
 app.use('/api',serviciosRoutes);
+app.use('/api',ventasRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
