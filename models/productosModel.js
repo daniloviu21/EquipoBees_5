aae7cd83eb74537f060ce52e5426e1e4f1a230d4
daniloviu21 <daniel.darkwatch@gmail.com>
2024-10-09 22:49:41 -0600

    fix(models): corregir consultas para que reciba id de las otras tablas

--- models/productosModel.js
+++ models/productosModel.js
@@ -8,14 +8,14 @@ class Productos {
     }
 
     static async create(data){
-        const {nombreProducto, descripcion, precio, stock} = data;
-        const result = await pool.query('INSERT INTO productos (nombreProducto, descripcion, precio, stock) VALUES ($1, $2, $3, $4) RETURNING *',[nombreProducto, descripcion, precio, stock]);
+        const {nombreProducto, descripcion, precio, stock, idMarca, idCategoria} = data;
+        const result = await pool.query('INSERT INTO productos (nombreProducto, descripcion, precio, stock, idMarca, idCategoria) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',[nombreProducto, descripcion, precio, stock, idMarca, idCategoria]);
         return result.rows[0];
     }
 
     static async update(id, data){
-        const {nombreProducto, descripcion, precio, stock} = data;
-        const result = await pool.query('UPDATE productos SET nombreProducto = $1, descripcion = $2, precio = $3, stock = $4, updated_at = now() WHERE id = $5 and deleted_at is null RETURNING *', [nombreProducto, descripcion, precio, stock, id]);
+        const {nombreProducto, descripcion, precio, stock, idMarca, idCategoria} = data;
+        const result = await pool.query('UPDATE productos SET nombreProducto = $1, descripcion = $2, precio = $3, stock = $4, idMarca = $5, idCategoria = $6, updated_at = now() WHERE id = $7 and deleted_at is null RETURNING *', [nombreProducto, descripcion, precio, stock, idMarca, idCategoria, id]);
         return result.rows[0];
     }
 
